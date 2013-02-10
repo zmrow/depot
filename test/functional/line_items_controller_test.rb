8f8f4fd4e433134d43270dadeafd312a7fbac74c
Zac Mrowicki <zmrowicki@gmail.com>
2013-02-10 15:59:59 -0800

    Iteration F5: Making Images Clickable

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -21,7 +21,7 @@ class LineItemsControllerTest < ActionController::TestCase
       post :create, product_id: products(:ruby).id
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path
   end
 
   test "should show line_item" do
@@ -37,6 +37,17 @@ class LineItemsControllerTest < ActionController::TestCase
   test "should update line_item" do
     put :update, id: @line_item, line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id }
     assert_redirected_to line_item_path(assigns(:line_item))
+  end
+
+  test "shold create line_item with ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end
+
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
   end
 
   test "should destroy line_item" do
