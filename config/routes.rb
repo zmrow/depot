b0b9683dc1eb0cda6468c27d9c6e8ad9e5601959
Zac Mrowicki <zmrowicki@gmail.com>
2013-02-12 21:40:20 -0800

    Iteration G2: Atom Feeds

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Depot::Application.routes.draw do
 
   get "store/index"
 
-  resources :products
+  resources :products do
+  	get :who_bought, on: :member
+  end
   root to: 'store#index', as: 'store'
 end
