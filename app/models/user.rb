2abcd6cfa3ffa325167545fce3a484df8b505c58
Zac Mrowicki <zmrowicki@gmail.com>
2013-02-24 13:11:58 -0800

    Iteration I4 Adding a Sidebar, More administration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
+	after_destroy :ensure_an_admin_remains
+
   attr_accessible :name, :password, :password_confirmation
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  private
+  	def ensure_an_admin_remains
+  		if User.count.zero?
+  			raise "Can't delete last user"
+  		end
+  	end
 end
