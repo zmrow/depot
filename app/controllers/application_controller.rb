6eb1cde202b16f7d9907c7fc2114f7135f2c313e
Zac Mrowicki <zmrowicki@gmail.com>
2013-03-04 17:49:17 -0800

    Iteration J1: Selecting the Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_i18n_locale_from_params
 	before_filter :authorize
   protect_from_forgery
 
   private
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.include?(params[:locale].to_sym)
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
 
   	def current_cart
   		Cart.find(session[:cart_id])
--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,14 @@ Depot::Application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  resources :users
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
-  get "store/index"
-
-  resources :products do
-  	get :who_bought, on: :member
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products do
+      get :who_bought, on: :member
+    end
+    root to: 'store#index', as: 'store'
   end
-  root to: 'store#index', as: 'store'
 end
